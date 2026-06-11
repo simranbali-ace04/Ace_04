a87c42b6a84b2691cdcdcf25ca5cb049d1f98701
Simran Bali <simranbali2006@gmail.com>
2026-06-11 16:41:09 +0530

    Hero Image added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ShapeGrid from "./components/ShapeGrid";
+import HeroDarkImg from "./components/HeroDarkImg";
+
 
 const App = () => {
   return (
@@ -14,6 +16,7 @@ const App = () => {
         />
       </div>
       <div className="w-3/5 min-h-screen relative z-10 rounded-2xl border border-zinc-800 bg-black/70 backdrop-blur-md text-white my-20">
+      <HeroDarkImg/>
       <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste, molestias?</h1>
       </div>
     </div>
---
+++ src/components/HeroDarkImg.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import HeroDark from "../assets/HeroDark.png";
+import HeroLight from "../assets/HeroLight.png";
+
+
+const HeroDarkImg = () => {
+  return (
+    <div className='w-full flex justify-center'>
+      <img src={HeroDark} alt="Dark Mode Hero Background"
+      className='flex object-cover p-1 rounded-2xl'/>
+    </div>
+  )
+}
+
+export default HeroDarkImg
